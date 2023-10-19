50e030fb53772e0f672af8e60e93380a02ca94e2
Dominik Ryryry <ereence@gmail.com>
2023-10-19 23:10:38 +0200

    Add project files.

---
+++ FitnessPanelMVC.Domain/Model/WorkoutExercise.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessPanelMVC.Domain.Model
+{
+    public class WorkoutExercise
+    {
+    }
+}
